dbb8328e0a3170c60c35da88daa40cb233d42588
Tomas Baca <klaxalk@gmail.com>
2023-10-21 16:14:03 +0200

    updated build script, added changelog

--- .ci/build_package.sh
+++ .ci/build_package.sh
@@ -7,13 +7,21 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: building the package"
 
-ARTIFACTS_FOLDER=$1
+VARIANT=$1
+ARTIFACTS_FOLDER=$2
 
 sudo apt-get -y install dpkg-dev
 
 echo "$0: building the package into '$ARTIFACTS_FOLDER'"
 
 mkdir -p $ARTIFACTS_FOLDER
+
+epoch=1
+# SHA=$(git rev-parse --short HEAD)
+build_flag=$(date +%Y%m%d.%H%M%S)
+
+sed -i "s/(/($epoch:/" ./package/DEBIAN/changelog
+sed -i "s/)/.${build_flag})/" ./package/DEBIAN/changelog
 
 dpkg-deb --build --root-owner-group package
 
